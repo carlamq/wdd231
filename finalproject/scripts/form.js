0f7cdfb1077095957d579560c3ae0b4f212df564
CarlaMQ <carlaquintanar1907@gmail.com>
2025-08-14 17:45:10 +0900

    project

--- finalproject/scripts/form.js
+++ finalproject/scripts/form.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('timestamp').value = new Date().toLocaleString('en-US');
 });
+
 //get the parameters from the URL
 const formInfo = new URLSearchParams(window.location.search);
 
 document.querySelector('#form-data').innerHTML = `
     <p>Message for: ${formInfo.get('first-name')} ${formInfo.get('last-name')}</p>
     <p>Email: ${formInfo.get('email')}</p>
+    <p>Phone: ${formInfo.get('phone')}</p>
+    <p>Contact Reason: ${formInfo.get('contact-reason')}</p>
     <p>Message: ${formInfo.get('message')}</p>
     <p>Submitted: ${formInfo.get('timestamp')}</p>
 `;
